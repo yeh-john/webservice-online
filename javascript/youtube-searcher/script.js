c4e33fa463d0ebf7ec56136faefceacf965d34de
John <88310061+yeh-john@users.noreply.github.com>
2022-06-05 00:40:55 +0800

    Update script.js

--- javascript/youtube-searcher/script.js
+++ javascript/youtube-searcher/script.js
@@ -3,15 +3,25 @@ function search(){
     
     // define search_bar
     let search_bar = document.querySelector(".search-bar").value;
-    
-    // Change space to special character
-    let search_info = search_bar.replace(/\s+/g, "+");
-    
-    // Chage search value to url
-    let url = `https://youtube.com/results?search_query=${search_info}`;
-    
-    // Jump to url
-    setTimeout(() => location.replace(url), 1000);
+
+    console.log(search_bar);
+
+    if (search_bar.length == 0) {
+        var input = document.querySelector('.search-bar');
+        input.placeholder='PLEASE ENTER TEXT';
+        // Change placeholder color
+    } else {
+        // Change space to special character
+        let search_info = search_bar.replace(/\s+/g, "+");
+        
+        // Chage search value to url
+        let url = `https://youtube.com/results?search_query=${search_info}`;
+        
+        // Jump to url
+        setTimeout(() => location.replace(url), 1000);
+    }
+
+
 
 };
 
